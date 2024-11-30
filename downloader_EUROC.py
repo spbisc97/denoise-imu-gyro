ca904ad6cb9cc912296ce5a8893d9eb6fcc54471
spbisc97 <rotondi97simone@gmail.com>
2024-11-30 17:03:42 +0100

    fix , add download

--- downloader_EUROC.py
+++ downloader_EUROC.py
@@ -8,16 +8,17 @@ EUROC_BASE_URL = "http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/"
 
 # List of dataset paths to download
 DATASETS = [
-    "machine_hall/MH_01_easy/MH_01_easy.zip",
-    "machine_hall/MH_02_easy/MH_02_easy.zip",
-    "machine_hall/MH_03_medium/MH_03_medium.zip",
-    "machine_hall/MH_04_difficult/MH_04_difficult.zip",
-    "vicon_room1/V1_01_easy/V1_01_easy.zip",
-    "vicon_room1/V1_02_medium/V1_02_medium.zip",
-    "vicon_room1/V1_03_difficult/V1_03_difficult.zip",
-    "vicon_room2/V2_01_easy/V2_01_easy.zip",
-    "vicon_room2/V2_02_medium/V2_02_medium.zip",
-    "vicon_room2/V2_03_difficult/V2_03_difficult.zip",
+    #"machine_hall/MH_01_easy/MH_01_easy.zip",
+    #"machine_hall/MH_02_easy/MH_02_easy.zip",
+    #"machine_hall/MH_03_medium/MH_03_medium.zip",
+    #"machine_hall/MH_04_difficult/MH_04_difficult.zip",
+    #"machine_hall/MH_05_difficult/MH_05_difficult.zip"
+    #"vicon_room1/V1_01_easy/V1_01_easy.zip",
+    #"vicon_room1/V1_02_medium/V1_02_medium.zip",
+    #"vicon_room1/V1_03_difficult/V1_03_difficult.zip",
+    #"vicon_room2/V2_01_easy/V2_01_easy.zip",
+    #"vicon_room2/V2_02_medium/V2_02_medium.zip",
+    #"vicon_room2/V2_03_difficult/V2_03_difficult.zip",
 ]
 
 # Folders for downloaded files and extracted data
